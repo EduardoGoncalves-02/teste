8c55d03fd7e628903d9c32079a5f455e3526885c
Julia <Julia@DESKTOP-B09K1T9>
2026-05-09 18:26:59 -0300

    init: Início do projeto

---
+++ README.md
@@ -0,0 +1,19 @@
+# Sistema de Controle de Estoque
+
+# Integrantes do Projeto:
+
+-Julia Dick, RA 1072615460, usuário do Github juliadick-blip
+-Eduardo Daniel G. Pereira, RA 10726110334, usuário do Github EduardoGoncalves-02
+-Melissa Mel Monteiro, RA 1072616173, usuário do Github melmonteirosilva
+-João Trilha, RA 10722618828, usuário do Github TrilhaX 
+
+## Sobre o Projeto
+Este projeto consiste no desenvolvimento de um sistema de controle de estoque em
+Java, criado para atender os requisitos da atividade A3 da unidade curricular de
+Algoritmos e Programação da UNISUL.
+
+O sistema tem como objetivo auxiliar uma empresa fictícia no gerenciamento de 
+produtos armazenados em estoque, permitindo o cadastro, movimentação, reajuste 
+de preços e emissão de relatórios.
+
+Desenvolvido utilizado JDK 25 e NetBeans 29.
---
+++ src/Principal.java
@@ -0,0 +1,201 @@
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+    static String[] nomes = new String[100];
+    static double[] precos = new double[100];
+    static String[] unidades = new String[100];
+    static int[] quantidades = new int[100];
+
+    static int totalProdutos = 0;
+
+    public static void main(String[] args) {
+        menuPrincipal();
+    }
+
+    // TELA 1.0
+    public static void menuPrincipal() {
+
+        int opcao;
+
+        do {
+
+            opcao = Integer.parseInt(
+                    JOptionPane.showInputDialog(
+                            "XYZ COMERCIO DE PRODUTOS LTDA.\n"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE\n"
+                            + "MENU PRINCIPAL\n"
+                            + "1 - CADASTRO DE PRODUTOS\n"
+                            + "2 - MOVIMENTAÇÃO\n"
+                            + "3 - REAJUSTE DE PREÇOS\n"
+                            + "4 - RELOTÓRIOS\n"
+                            + "0 - FINALIZAR\n"
+                            + "OPÇÃO: "
+                    )
+            );
+
+            switch (opcao) {
+
+                case 1:
+                    menuCadastro();
+                    break;
+
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Sistema finalizado.");
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida.");
+            }
+
+        } while (opcao != 0);
+    }
+
+    // TELA 1.1
+    public static void menuCadastro() {
+
+        int opcao;
+
+        do {
+
+            opcao = Integer.parseInt(
+                    JOptionPane.showInputDialog( "XYZ COMERCIO DE PRODUTOS LTDA.\n"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE\n"
+                            + "CADASTRO DE PRODUTOS\n"
+                            + "1 - INCLUSÃO\n"
+                            + "2 - ALTERAÇÃO\n"
+                            + "3 - CONSULTA\n"
+                            + "4 - EXCLUSÃO\n"
+                            + "0 - RETORNAR\n"
+                            + "OPÇÃO: "
+                    )
+            );
+
+            switch (opcao) {
+
+                case 1:
+                    incluirProduto();
+                    break;
+
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Retornando ao menu principal...");
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida.");
+            }
+
+        } while (opcao != 0);
+    }
+
+    // TELA 1.1.1
+    public static void incluirProduto() {
+
+        char novaInclusao;
+        String nome;
+        double preco;
+        String unidade;
+        int quantidade;
+        char confirma;
+
+        do {
+
+            // NOME
+
+            while (true) {
+
+                nome = JOptionPane.showInputDialog(
+                        "INCLUSÃO DE PRODUTO\n"
+                        + "NOME: "
+                );
+
+                if (produtoExiste(nome)) {
+
+                    JOptionPane.showMessageDialog(null, "ERRO: Produto já cadastrado.");
+
+                } else {
+                    break;
+                }
+            }
+            
+            // PREÇO
+            while (true) {
+
+                preco = Double.parseDouble(
+                        JOptionPane.showInputDialog("PREÇO: ")
+                );
+
+                if (preco <= 0) {
+
+                    JOptionPane.showMessageDialog(null, "ERRO: O preço deve ser maior que zero.");
+
+                } else {
+                    break;
+                }
+            }
+
+            // UNIDADE
+            unidade = JOptionPane.showInputDialog("UNIDADE: ");
+
+            // QUANTIDADE
+            while (true) {
+
+                quantidade = Integer.parseInt(
+                        JOptionPane.showInputDialog("QUANTIDADE: ")
+                );
+
+                if (quantidade < 0) {
+
+                    JOptionPane.showMessageDialog(null, "ERRO: Quantidade inválida.");
+
+                } else {
+                    break;
+                }
+            }
+
+            // ==========================
+            // CONFIRMAÇÃO
+            // ==========================
+            confirma = JOptionPane.showInputDialog(
+                    "CONFIRMA INCLUSÃO (S/N)?"
+            ).toUpperCase().charAt(0);
+
+            if (confirma == 'S') {
+
+                nomes[totalProdutos] = nome;
+                precos[totalProdutos] = preco;
+                unidades[totalProdutos] = unidade;
+                quantidades[totalProdutos] = quantidade;
+
+                totalProdutos++;
+
+                JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+            } else {
+
+                JOptionPane.showMessageDialog(null, "Cadastro cancelado.");
+            }
+            
+            // SE SIM PARA NOVA INCLUSÃO
+            novaInclusao = JOptionPane.showInputDialog(
+                    "NOVA INCLUSÃO (S/N)?"
+            ).toUpperCase().charAt(0);
+
+        } while (novaInclusao == 'S');
+    }
+
+
+    // SUBROTINA DE VALIDAÇÃO SE O PRODUTO EXISTE
+    public static boolean produtoExiste(String nome) {
+
+        for (int i = 0; i < totalProdutos; i++) {
+
+            if (nomes[i].equalsIgnoreCase(nome)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
